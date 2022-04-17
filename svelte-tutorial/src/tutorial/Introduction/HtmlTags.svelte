a9d78c91e925b2598d3c21b5b754735b33a7ffa1
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-04-17 10:37:53 +0900

    feat: HTML tags

---
+++ svelte-tutorial/src/tutorial/Introduction/HtmlTags.svelte
@@ -0,0 +1,6 @@
+<script>
+  let string = `this string contains some <strong>HTML!!!</strong>`
+</script>
+
+<!-- @htmlを使用することで、html描画で表示することができる。 -->
+<p>{@html string}</p>
--- svelte-tutorial/src/tutorial/Introduction/Introduction.svelte
+++ svelte-tutorial/src/tutorial/Introduction/Introduction.svelte
@@ -1,8 +1,10 @@
 <script>
   import DynamicAttributes from './DynamicAttributes.svelte'
+  import HtmlTags from './HtmlTags.svelte'
 </script>
 
 <div>
   <h2>Introduction</h2>
   <DynamicAttributes />
+  <HtmlTags />
 </div>
