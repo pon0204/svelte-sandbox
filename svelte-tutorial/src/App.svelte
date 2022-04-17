1c9d8cb973e84b95ad97cc0231b0386fd39347d4
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-04-17 11:06:50 +0900

    feat: 2まで

--- svelte-tutorial/src/App.svelte
+++ svelte-tutorial/src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import Introduction from './tutorial/Introduction/Introduction.svelte'
+  import Introduction from './tutorial/1-Introduction/Introduction.svelte'
+  import Reactivity from './tutorial/2-Reactivity/Reactivity.svelte'
 </script>
 
 <main>
+  <!-- 1-Introduction -->
   <Introduction />
+  <Reactivity />
 </main>
---
+++ svelte-tutorial/src/tutorial/2-Reactivity/Reactivity.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = 0
+
+  function incrementCount() {
+    count += 1
+  }
+  $: doubled = count * 2
+</script>
+
+<div>
+  <h2>Reactivity</h2>
+  <button on:click={incrementCount}>
+    Clicked {count}
+    {count === 1 ? 'time' : 'times'}
+  </button>
+  <p>{count} doubled is {doubled}</p>
+</div>
